4f0a966b12c65fe2dae3d3620f5827097a44a077
Luke <luke@Lukes-MacBook-Pro.local>
2024-02-13 20:16:32 +0000

    Initial springboot configuration

---
+++ src/test/java/com/example/MeteorIO/MeteorIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MeteorIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteorIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
